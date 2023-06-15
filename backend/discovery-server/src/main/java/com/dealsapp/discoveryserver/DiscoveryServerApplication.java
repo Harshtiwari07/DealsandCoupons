6783330c9ba761d54f8c7bdd6c5791c3f9880b0e
Harsh Tiwari <ht39091@gmail.com>
2023-06-15 13:51:06 +0530

    chnage2

--- backend/discovery-server/src/main/java/com/dealsapp/discoveryserver/DiscoveryServerApplication.java
+++ backend/discovery-server/src/main/java/com/dealsapp/discoveryserver/DiscoveryServerApplication.java
@@ -3,7 +3,7 @@ package com.dealsapp.discoveryserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//Discovery Client
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServerApplication {
